5b9e7413fe547ad25e460a3b7c885d214bf4692c
NoahG6190 <noahg258@nycstudents.net>
2025-10-31 11:00:30 -0700

    new code 10/31/25

---
+++ index.js
@@ -0,0 +1,23 @@
+const songs = [
+    { title: "Song 1", artist: "Artist A", cover: "cover1.jpg"},
+    { title: "Song 2", artist: "Artist B", cover: "cover2.jpg" },
+    { title: "Song 3", artist: "Artist C",cover: "cover3.jpg" },
+    { title: "Song 4", artist: "Artist D",cover: "cover4.jpg" },
+    { title: "Song 5", artist: "Artist E",cover: "cover5.jpg" },
+    { title: "Song 6", artist: "Artist F",cover: "cover6.jpg" },
+    { title: "Song 7", artist: "Artist G",cover: "cover7.jpg" },
+    { title: "Song 8", artist: "Artist H",cover: "cover8.jpg" },
+    { title: "Song 9", artist: "Artist I",cover: "cover9.jpg" },
+    { title: "Song 10", artist: "Artist J",cover: "cover10.jpg" },
+];
+
+const playlistContainer = document.getElementById('playlist-container');
+const songContainer = document.getElementById('song-container');
+let playlist = [];
+ function inject(songs) {
+    songContainer.insertAdjacentHTML
+    ('beforeend',
+        <div class="card">
+            
+    )
+
