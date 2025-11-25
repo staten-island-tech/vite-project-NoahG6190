9deb0da088816a2a654cae75c569ea0bba599428
NoahG6190 <noahg258@nycstudents.net>
2025-11-25 10:59:56 -0800

    Update main.js

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,16 +1,20 @@
 import "./style.css";
 
 const song = [
-  { title: "Song 1", artist: "Artist A", cover: "cover1.jpg" },
-  { title: "Song 2", artist: "Artist B", cover: "cover2.jpg" },
-  { title: "Song 3", artist: "Artist C", cover: "cover3.jpg" },
-  { title: "Song 4", artist: "Artist D", cover: "cover4.jpg" },
-  { title: "Song 5", artist: "Artist E", cover: "cover5.jpg" },
-  { title: "Song 6", artist: "Artist F", cover: "cover6.jpg" },
-  { title: "Song 7", artist: "Artist G", cover: "cover7.jpg" },
-  { title: "Song 8", artist: "Artist H", cover: "cover8.jpg" },
-  { title: "Song 9", artist: "Artist I", cover: "cover9.jpg" },
-  { title: "Song 10", artist: "Artist J", cover: "cover10.jpg" },
+  { title: "Lady of Namek", artist: "Tory Lanez", cover: "cover1.jpg" },
+  { title: "Girls Want Girls", artist: "Drake", cover: "cover2.jpg" },
+  { title: "Nothing Like Uuu", artist: "Nettspend", cover: "cover3.jpg" },
+  { title: "crush", artist: "2hollis", cover: "cover4.jpg" },
+  { title: "ATTITUDE", artist: "Don Toliver", cover: "cover5.jpg" },
+  { title: "Rather Lie", artist: "Playboi Carti", cover: "cover6.jpg" },
+  { title: "poison", artist: "jaydes", cover: "cover7.jpg" },
+  { title: "Snooze", artist: "SZA", cover: "cover8.jpg" },
+  { title: "yes", artist: "Ken Carson", cover: "cover9.jpg" },
+  {
+    title: "Spin Bout U",
+    artist: "Drake feat. 21 Savage",
+    cover: "cover10.jpg",
+  },
 ];
 
 const playlistContainer = document.getElementById("playlist-container");
