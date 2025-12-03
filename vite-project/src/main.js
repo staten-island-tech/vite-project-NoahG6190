7f3fb6e02fedd837e4dc737765af664fa9206a20
NoahG6190 <noahg258@nycstudents.net>
2025-12-03 10:26:57 -0800

    Update main.js

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -116,7 +116,12 @@ backToSongsBtn.addEventListener("click", () => {
   goToPlaylistBtn.style.display = "block";
   backToSongsBtn.style.display = "none";
 });
-document.querySelector(".btn").addEventListener("click", () => {
-  document.body.classList.toggle("cool");
-  document.body.classList.toggle("warm");
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  }
 });
